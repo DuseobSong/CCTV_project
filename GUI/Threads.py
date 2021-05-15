098b2305e283ac4940558f301685dc871ea7c02b
DuseobSong <duseob.song@outlook.kr>
2021-05-15 17:15:10 +0900

    Joystick added, implementation not completed

--- GUI/Threads.py
+++ GUI/Threads.py
@@ -1,8 +1,10 @@
 import sys, cv2, imagezmq, socket, requests, pickle
 from io import BytesIO
 import numpy as np
-from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
-from PyQt5.QtGui import QImage, QPixmap
+from enum import Enum
+from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot, QPointF, QPoint, QRectF, QLineF
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QImage, QPixmap, QPainter
 
 
 # classes for video streaming
@@ -130,4 +132,107 @@ class UDPStreaming(Streaming):
                             self.changePixmap.emit(image)
                         else:
                             self.changePixmap.emit(self.dummy)
+
+# Joystick - CameraControl
+class Direction(Enum):
+    Left = 0
+    Right = 1
+    Up = 2
+    Down = 3
+    LeftUp = 4
+    LeftDown = 5
+    RightUp = 6
+    RightDown = 7
+
+class Joystick(QWidget):
+    send_cmd = pyqtSignal(int, int)
+
+    def __init__(self, parent=None):
+        super(Joystick, self).__init__(parent)
+        self.setMinimumSize(150, 150)
+        self.moving_offset=(QPoint(0, 0))
+        self.grab_center = False
+        self.__max_distance = 70
+        self.init_pwm_x = 993
+        self.init_pwn_y = 813
+        self.cur_pwm_x = self.init_pwm_x
+        self.cur_pwm_y = self.init_pwm_y
+        self.prev_pwm_x = self.init_pwm_x
+        self.prev_pwm_y = self.init_pwm_y
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        bounds = QRectF(-self.__max_distance, -self.__max_distance, self.__max_distance*2, self.__max_distance*2).translated(self._center())
+        bounds2 = QRectF(-30, -30, 60, 60).translated(self._center())
+        painter.drawEllipse(bounds)
+        painter.drawEllipse(bounds2)
+        painter.setBrush(Qt.black)
+        painter.dwawEllipse(self._center_ellipse())
+
+    def _center_ellipse(self):
+        if self.grab_center:
+            return QRectF(-20, -20, 40, 40).translated(self.moving_offset)
+        return QRectF(-20, -20, 40, 40).translated(self._center())
+
+    def _center(self):
+        return QPointF(self.width()/2, self.height()/2)
+
+    def _bound_joystick(self, point):
+        limitLine = QLineF(self._center(), point)
+        if(limitLine.length() > self.__max_distance):
+            limitLine.setLength(self.__max_distance)
+        return limitLine.p2()
+
+    def joystick_direction(self):
+        if not self.grab_center:
+            return 0;
+        norm_vector = QLineF(self._center(), self.moving_offset)
+        current_distance = norm_vector.length()
+        angle = norm_vector.angle()
+
+        distance = min(current_distance / self.__max_distance, 1.0)
+        if 22.5 <= angle < 67.5:
+
+            return (Direction.RightUp, distance)
+        elif 67.5 <= angle < 112.5:
+            return (Direction.up, distance)
+        elif 112.5 <= angle < 157.5:
+            return (Direction.LeftUp, distance)
+        elif 157.5 <= angle < 202.5:
+            return (Direction.Left, distance)
+        elif 202.5 <= angle < 247.5:
+            return (Direction.LeftDown, distance)
+        elif 247.5 <= angle < 292.5:
+            return (Direction.Down, distance)
+        elif 292.5 <= angle < 337.5:
+            return (Direction.RigntDown, distance)
+        return (Direction.Right, distance)
+
+    def mousePressEvent(self, ev):
+        self.grab_center = self._center_ellipse().contains(ev.pos())
+        return super().mousePressEvent(ev)
+
+    def mouseReleaseEvent(self, event):
+        self.grab_center = False
+        self.moving_offset = QPointF(0, 0)
+        self.update()
+
+    def mouseMoveEvent(self, event):
+        if self.grab_center:
+            print("Moving")
+            self.moving_offset = self._bound_joystick(event.pos())
+            self.update()
+        print(self.joystick_direction())
+
+class CameraControl(QThread):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.joystick = Joystick(self)
+
+    def run(self):
+        pass
+
+    @pyqtSlot(int, int)
+    def send_ctrl_cmd(self, x, y):
+        pass
 
